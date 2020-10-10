f995c0a3b302878a638b812b827556a8a6fa91f7
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-10-10 22:27:53 +0300

    task3 for review

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+e
+5task3/src/main/java/by/training/task3/MainActivity.kt,6\f\6f83cc019d29de5133b2b651e55bc185f927d9a0
---
+++ task3/src/main/java/by/training/task3/MainActivity.kt
@@ -0,0 +1,12 @@
+package by.training.task3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.constraint_layout_d3_part2)
+    }
+}
