c039696d88b61ea31165b180003adf1f35f5d172
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-10-23 20:51:20 +0300

    Add Contact data class

---
+++ task4/src/main/java/by/training/task4/entity/Contact.kt
@@ -0,0 +1,7 @@
+package by.training.task4.entity
+
+data class Contact(
+    val imageResource: Int,
+    val contactName: String,
+    val contact: String
+)
---
+++ task4/src/main/java/by/training/task4/model/ContactsViewModel.kt
@@ -0,0 +1,7 @@
+package by.training.task4.model
+
+import androidx.lifecycle.ViewModel
+
+class ContactsViewModel : ViewModel() {
+//    private val contacts = MutableLiveData<List<>>
+}
