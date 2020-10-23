12e7223261c9d7003c123c7705d58d5fff1684f6
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-10-23 17:24:35 +0300

    Moove activities to activity package

--- task4/src/main/java/by/training/task4/activity/AddContactActivity.kt
+++ task4/src/main/java/by/training/task4/activity/AddContactActivity.kt
@@ -1,11 +1,12 @@
-package by.training.task4
+package by.training.task4.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import by.training.task4.R
 
-class MainActivity : AppCompatActivity() {
+class AddContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add_contact)
     }
 }
---
+++ task4/src/main/java/by/training/task4/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package by.training.task4.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import by.training.task4.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
