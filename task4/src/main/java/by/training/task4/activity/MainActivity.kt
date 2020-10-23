69bb7d8bec83ad607f644f29787f282d9420261e
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-10-23 18:33:29 +0300

    Add id's to radio buttons activity_add_contact.xml

--- task4/src/main/java/by/training/task4/activity/MainActivity.kt
+++ task4/src/main/java/by/training/task4/activity/MainActivity.kt
@@ -1,13 +1,22 @@
 package by.training.task4.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import by.training.task4.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
     }
 
+    private fun init() {
+        floatingActionButton.setOnClickListener {
+            startActivity(Intent(this, AddContactActivity::class.java))
+        }
+    }
 }
