9f9d958819e9cf85fe917171bf0696c1de58cbec
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-12-09 22:34:40 +0300

    add DtoModule class

--- task9mvvm/src/main/java/by/itacademy/training/task9mvvm/di/component/AppComponent.kt
+++ task9mvvm/src/main/java/by/itacademy/training/task9mvvm/di/component/AppComponent.kt
@@ -1,12 +1,13 @@
 package by.itacademy.training.task9mvvm.di.component
 
 import by.itacademy.training.task9mvvm.di.module.AppContextModule
+import by.itacademy.training.task9mvvm.di.module.DtoModule
 import by.itacademy.training.task9mvvm.di.module.NetworkModule
 import by.itacademy.training.task9mvvm.ui.viewmodel.MainViewModel
 import dagger.Component
 import javax.inject.Singleton
 
-@Component(modules = [AppContextModule::class, NetworkModule::class])
+@Component(modules = [AppContextModule::class, NetworkModule::class, DtoModule::class])
 @Singleton
 interface AppComponent {
 
--- task9mvvm/src/main/java/by/itacademy/training/task9mvvm/ui/viewmodel/MainViewModel.kt
+++ task9mvvm/src/main/java/by/itacademy/training/task9mvvm/ui/viewmodel/MainViewModel.kt
@@ -3,17 +3,26 @@ package by.itacademy.training.task9mvvm.ui.viewmodel
 import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import by.itacademy.training.task9mvvm.di.component.DaggerAppComponent
+import by.itacademy.training.task9mvvm.model.entity.WeatherReport
 import by.itacademy.training.task9mvvm.model.repository.WeatherForecastRepositoryImpl
+import by.itacademy.training.task9mvvm.util.DTOMapper
+import by.itacademy.training.task9mvvm.util.Event
+import by.itacademy.training.task9mvvm.util.Status
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-    @Inject
-    lateinit var weatherForecastRepository: WeatherForecastRepositoryImpl
+    @Inject lateinit var weatherForecastRepository: WeatherForecastRepositoryImpl
+    @Inject lateinit var dtoMapper: DTOMapper
+
+    private var _weatherReportData = MutableLiveData<Event<WeatherReport>>()
+    val weatherReportData: LiveData<Event<WeatherReport>> = _weatherReportData
 
     init {
         DaggerAppComponent.create().inject(this)
@@ -22,8 +31,14 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     private fun fetchData() {
         viewModelScope.launch(Dispatchers.IO) {
-            val result = weatherForecastRepository.getWeatherForecastForDay("Minsk")
-            Log.d("TAG", result.toString())
+            _weatherReportData.postValue(Event(Status.LOADING, null, null))
+            try {
+                val result = weatherForecastRepository.getWeatherForecastForDay("Minsk")
+                val weatherReport = dtoMapper.invoke(result)
+                _weatherReportData.postValue(Event(Status.SUCCESS, weatherReport, null))
+            } catch (exception: Exception) {
+                _weatherReportData.postValue(Event(Status.ERROR, null, exception.message))
+            }
         }
     }
 }
