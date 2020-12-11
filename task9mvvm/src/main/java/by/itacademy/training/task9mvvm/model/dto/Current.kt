5b3ac90f1f0ce2fbd77ed7999f8f2831006e17ac
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-12-11 17:57:43 +0300

    add Dto class Condition

--- task9mvvm/src/main/java/by/itacademy/training/task9mvvm/model/dto/Current.kt
+++ task9mvvm/src/main/java/by/itacademy/training/task9mvvm/model/dto/Current.kt
@@ -3,4 +3,5 @@ package by.itacademy.training.task9mvvm.model.dto
 data class Current(
     val temp_c: Double,
     val temp_f: Double,
+    val condition: Condition
 )
--- task9mvvm/src/main/java/by/itacademy/training/task9mvvm/model/repository/WeatherForecastRepositoryImpl.kt
+++ task9mvvm/src/main/java/by/itacademy/training/task9mvvm/model/repository/WeatherForecastRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package by.itacademy.training.task9mvvm.model.repository
 
+import android.util.Log
 import by.itacademy.training.task9mvvm.model.api.WeatherApi
 import by.itacademy.training.task9mvvm.util.DTOMapper
 import kotlinx.coroutines.Dispatchers
@@ -13,6 +14,7 @@ class WeatherForecastRepositoryImpl @Inject constructor(
 
     override suspend fun getWeatherForecastForDay(city: String) = withContext(Dispatchers.IO) {
         val requestResult = api.getCurrentWeather(city)
+        Log.d("TAG", requestResult.toString())
         dtoMapper.invoke(requestResult)
     }
 }
