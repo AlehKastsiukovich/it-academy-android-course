1a7e89fa710221d6499d43b96489e0ebe72e80ba
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-12-13 12:32:20 +0300

    add CitiesDao class

--- task9mvvm/src/main/java/by/itacademy/training/task9mvvm/db/CitiesDao.kt
+++ task9mvvm/src/main/java/by/itacademy/training/task9mvvm/db/CitiesDao.kt
@@ -1,9 +1,18 @@
 package by.itacademy.training.task9mvvm.db
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import by.itacademy.training.task9mvvm.model.dto.db.CityDTO
+import by.itacademy.training.task9mvvm.model.dto.db.CityDTO.Companion.TABLE_NAME
 
 @Dao
 interface CitiesDao {
 
-    fun
+    @Query("SELECT * FROM $TABLE_NAME")
+    fun getAll(): LiveData<CityDTO>
+
+    @Insert
+    fun insert(cityDTO: CityDTO)
 }
--- task9mvvm/src/main/java/by/itacademy/training/task9mvvm/model/dto/db/CityDTO.kt
+++ task9mvvm/src/main/java/by/itacademy/training/task9mvvm/model/dto/db/CityDTO.kt
@@ -2,10 +2,15 @@ package by.itacademy.training.task9mvvm.model.dto.db
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import by.itacademy.training.task9mvvm.model.dto.db.CityDTO.Companion.TABLE_NAME
 
-@Entity(tableName = "city")
-class CityDTO(
+@Entity(tableName = TABLE_NAME)
+data class CityDTO(
     @PrimaryKey(autoGenerate = true)
     private val id: Int,
     private val name: String
-)
+) {
+    companion object {
+        const val TABLE_NAME = "city"
+    }
+}
