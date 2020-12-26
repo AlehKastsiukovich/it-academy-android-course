3e1bfc9ea15cb164c631d60f03a477fbd0784f00
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-12-26 19:16:36 +0300

    add LocalBinderImpl class

--- app/src/main/java/by/training/itacademycourse/service/AudioPlayerService.kt
+++ app/src/main/java/by/training/itacademycourse/service/AudioPlayerService.kt
@@ -3,10 +3,36 @@ package by.training.itacademycourse.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.os.IInterface
+import android.os.Parcel
+import java.io.FileDescriptor
 
 class AudioPlayerService : Service() {
 
-    override fun onBind(intent: Intent?): IBinder? {
-        TODO("Not yet implemented")
+    private val binder: LocalBinder = LocalBinderImpl()
+
+    inner class LocalBinderImpl : LocalBinder {
+
+        override fun getService(): AudioPlayerService = this@AudioPlayerService
+
+        override fun getInterfaceDescriptor(): String? = null
+
+        override fun pingBinder(): Boolean = false
+
+        override fun isBinderAlive(): Boolean = false
+
+        override fun queryLocalInterface(descriptor: String): IInterface? = null
+
+        override fun dump(fd: FileDescriptor, args: Array<out String>?) = Unit
+
+        override fun dumpAsync(fd: FileDescriptor, args: Array<out String>?) = Unit
+
+        override fun transact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean = false
+
+        override fun linkToDeath(recipient: IBinder.DeathRecipient, flags: Int) = Unit
+
+        override fun unlinkToDeath(recipient: IBinder.DeathRecipient, flags: Int): Boolean = false
     }
+
+    override fun onBind(intent: Intent?): IBinder? = binder
 }
